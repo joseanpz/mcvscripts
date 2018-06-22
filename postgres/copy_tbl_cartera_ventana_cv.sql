8b13299fdf54ebec71a0eda456946e714cd3c4b6
Jose Arce <joseangel.epzarce@gmail.com>
2018-06-22 16:09:28 -0500

    copy ventana cv

---
+++ postgres/copy_tbl_cartera_ventana_cv.sql
@@ -0,0 +1,19 @@
+﻿drop table if exists cartera.tbl_cartera_ventana_cv;
+
+create table cartera.tbl_cartera_ventana_cv (
+    llave_universal char(20),
+    rfc  varchar(15),
+    mes_ancla date,
+    fecha integer,
+    cartera_vencida double precision,
+    sdovdo_m1 double precision,
+    sdovdo_m2 double precision,
+    sdovdo_m3 double precision,
+    sdovdo_m4 double precision,
+    sdovdo_m5 double precision,
+    sdovdo_m6 double precision
+);
+
+copy cartera.tbl_cartera_ventana_cv 
+from '/home/jose/Desktop/Riesgo/projects/CRTV/data/cartera/cartera_vencida2_utf8.csv'
+delimiter '|' null '\N' csv;
