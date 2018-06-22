4ef7b2c6977a011c7288694ba06338a0ab7ca379
Jose Arce <joseangel.epzarce@gmail.com>
2018-06-22 11:28:02 -0500

    universo colocacion

---
+++ postgres/universo_colocacion_mensual_201401_201805.sql
@@ -0,0 +1,6 @@
+﻿select count(*), min(fecha), max(fecha) 
+from colocacion.tbl_colocacion_mensual_uen_peq_med
+where uen in ('05 PEQUEÑA EMPRESA NL', '09 PEQUEÑA EMPRESA TERRITORIAL') 
+and perfil_negocio_actual not in ('CORPORATIVO', 'DIRECCION', 'DUMMY', 'GOBIERNO')
+and producto not in ('3I ACS AUTOPRESTO', '3N ACS AUTOPRESTO', 'Z1 SEGURO ACS AUTOPRESTO')
+and subsegmento_producto <> 'AB - VARIOS'
