f0fe1afd7135816c199a07edb68b059d1191ac6a
Jose Arce <joseangel.epzarce@gmail.com>
2018-07-02 11:31:07 -0500

    conteos vintage

--- postgres/vintage/pct_cosechas_by_bucket.sql
+++ postgres/vintage/pct_cosechas_by_bucket.sql
@@ -1,14 +1,15 @@
 ﻿create view cartera.pct_cosechas_by_bucket as
 select 
     fecha_ancla, 
-    avg(sdovdo_m0) as pct0,
-    avg(sdovdo_m1) as pct1,
-    avg(sdovdo_m2) as pct2,
-    avg(sdovdo_m3) as pct3,
-    avg(sdovdo_m4) as pct4,
-    avg(sdovdo_m5) as pct5,
-    avg(sdovdo_m6) as pct6
+    round(100 * avg(sdovdo_m0), 2) as pct0,
+    round(100 * avg(sdovdo_m1), 2) as pct1,
+    round(100 * avg(sdovdo_m2), 2) as pct2,
+    round(100 * avg(sdovdo_m3), 2) as pct3,
+    round(100 * avg(sdovdo_m4), 2) as pct4,
+    round(100 * avg(sdovdo_m5), 2) as pct5,
+    round(100 * avg(sdovdo_m6), 2) as pct6,
+    count(*)
 from cartera.tbl_cosechas
-where bkt = 3 
+where bkt = 4 and monto_clasificacion = 2
 group by fecha_ancla
 order by fecha_ancla
