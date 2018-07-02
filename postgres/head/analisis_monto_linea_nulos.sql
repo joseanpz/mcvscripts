d0cdcbce3991ee5c1afb408582c5f0ae12a5cd15
Jose Arce <joseangel.epzarce@gmail.com>
2018-07-02 11:32:03 -0500

    indicadores vintage

---
+++ postgres/head/analisis_monto_linea_nulos.sql
@@ -0,0 +1,18 @@
+﻿select 
+    *,
+    array_length(montos_linea, 1) as length
+from (
+select 
+    llave_universal_20, 
+    array_agg(distinct monto_linea) as montos_linea, 
+    array_agg(distinct monto_original) as montos_originales    
+from cartera.tbl_universo
+where llave_universal_20 in (
+    select distinct llave_universal
+    from cartera.tbl_cosechas
+    where monto_clasificacion = 3
+)
+-- and (monto_linea >= 8000000 or monto_original >= 8000000)
+group by llave_universal_20
+) a
+order by length
---
+++ postgres/head/conteos_universo_sin_banderas.sql
@@ -0,0 +1,8 @@
+﻿select 
+fecha,
+count(*)
+from cartera.tbl_universo_banderas
+where cci_flag + reestructuras_flag + sobregiro_flag + 
+intercompania_flag + cartera_adquirida_flag = 0
+group by fecha
+order by fecha
