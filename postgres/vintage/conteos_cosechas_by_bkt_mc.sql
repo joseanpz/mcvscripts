780f12499a1e8f8eb8dad3c3bac02d039d5d1218
Jose Arce <joseangel.epzarce@gmail.com>
2018-07-06 10:04:44 -0500

    pct & cosechas por bucket

---
+++ postgres/vintage/conteos_cosechas_by_bkt_mc.sql
@@ -0,0 +1,46 @@
+﻿select 
+    a.fecha_ancla, 
+    a.mc0,
+    b.mc1,
+    c.mc2,
+    d.mc3,
+    e.total
+from (
+    select fecha_ancla, count(*) as mc0
+    from cartera.tbl_cosechas
+    where monto_clasificacion = 0
+        and bkt = 1
+    group by fecha_ancla
+) a
+left join (
+    select fecha_ancla, count(*) as mc1
+    from cartera.tbl_cosechas
+    where monto_clasificacion = 1
+    and bkt = 1
+    group by fecha_ancla
+) b
+on a.fecha_ancla = b.fecha_ancla
+left join (
+    select fecha_ancla, count(*) as mc2
+    from cartera.tbl_cosechas
+    where monto_clasificacion = 2
+    and bkt = 1
+    group by fecha_ancla
+) c
+on a.fecha_ancla = c.fecha_ancla
+left join (
+    select fecha_ancla, count(*) as mc3
+    from cartera.tbl_cosechas
+    where monto_clasificacion = 3
+    and bkt = 1
+    group by fecha_ancla
+) d
+on a.fecha_ancla = d.fecha_ancla
+left join (
+    select fecha_ancla, count(*) as total
+    from cartera.tbl_cosechas
+    where bkt = 1
+    group by fecha_ancla
+) e
+on a.fecha_ancla = e.fecha_ancla
+order by a.fecha_ancla
--- postgres/vintage/pct_cosechas_rfc_by_bucket.sql
+++ postgres/vintage/pct_cosechas_rfc_by_bucket.sql
@@ -1,13 +1,13 @@
 ﻿-- create view cartera.pct_cosechas_by_bucket as
 select 
     fecha_ancla, 
-    round(100 * avg(sdovdo_m0), 2) as pct0,
-    round(100 * avg(sdovdo_m1), 2) as pct1,
-    round(100 * avg(sdovdo_m2), 2) as pct2,
-    round(100 * avg(sdovdo_m3), 2) as pct3,
-    round(100 * avg(sdovdo_m4), 2) as pct4,
-    round(100 * avg(sdovdo_m5), 2) as pct5,
-    round(100 * avg(sdovdo_m6), 2) as pct6,
+    round(100 * avg(sdovdo_max_m0), 2) as pct0,
+    round(100 * avg(sdovdo_max_m1), 2) as pct1,
+    round(100 * avg(sdovdo_max_m2), 2) as pct2,
+    round(100 * avg(sdovdo_max_m3), 2) as pct3,
+    round(100 * avg(sdovdo_max_m4), 2) as pct4,
+    round(100 * avg(sdovdo_max_m5), 2) as pct5,
+    round(100 * avg(sdovdo_max_m6), 2) as pct6,
     count(*)
 from cartera.tbl_cosechas_rfc
 where bkt = 4 and monto_clasificacion = 2
