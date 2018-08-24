3ea0fca71611f7fde953b81bd424f4c016128822
Jose Arce <joseangel.epzarce@gmail.com>
2018-08-24 12:38:04 -0500

    fix missing months and 12m issue

--- mysql/LGG/refactor/postgres/cretae_tbl_vars_bal.sql
+++ mysql/LGG/refactor/postgres/cretae_tbl_vars_bal.sql
@@ -12,10 +12,10 @@ select
   a.mesesdc,
   b.saldo,
   max(c.saldo) as max_bal_03m,
-  min(c.saldo) as min_bal_03m,
-  avg(c.saldo) as avg_bal_03m,
-  sum(c.saldo * (1 + (a.mesesdc - c.mesesdc)) / 10) as avg_pnd_menos_bal_03m,
-  sum(c.saldo * (4 - (a.mesesdc - c.mesesdc)) / 10) as avg_pnd_mas_bal_03m
+  min(c.saldo) as min_bal_03m,  
+  sum(c.saldo * (1 + (a.mesesdc - c.mesesdc))) / sum(1 + (a.mesesdc - c.mesesdc))  as avg_pnd_menos_bal_03m,
+  sum(c.saldo * (4 - (a.mesesdc - c.mesesdc))) / sum(4 - (a.mesesdc - c.mesesdc)) as avg_pnd_mas_bal_03m,
+  avg(c.saldo) as avg_bal_03m
 from 
   cartera.tbl_universo a
   left join cartera.tbl_cartera_comercial_rfc_saldo b
@@ -40,10 +40,10 @@ create table if not exists
 select
   a.*,
   max(b.saldo) as max_bal_06m,
-  min(b.saldo) as min_bal_06m,
-  avg(b.saldo) as avg_bal_06m,
-  sum(b.saldo * (1 + (a.mesesdc - b.mesesdc)) / 28) as avg_pnd_menos_bal_06m,
-  sum(b.saldo * (7 - (a.mesesdc - b.mesesdc)) / 28) as avg_pnd_mas_bal_06m
+  min(b.saldo) as min_bal_06m,  
+  sum(b.saldo * (1 + (a.mesesdc - b.mesesdc))) / sum(1 + (a.mesesdc - b.mesesdc))  as avg_pnd_menos_bal_06m,
+  sum(b.saldo * (7 - (a.mesesdc - b.mesesdc))) / sum(7 - (a.mesesdc - b.mesesdc)) as avg_pnd_mas_bal_06m,
+  avg(b.saldo) as avg_bal_06m
 from 
   cartera.tbl_vars_bal_step1 a
   left join cartera.tbl_cartera_comercial_rfc_saldo b
@@ -67,14 +67,14 @@ create table if not exists
 select
   a.*,
   max(b.saldo) as max_bal_12m,
-  min(b.saldo) as min_bal_12m,
-  avg(b.saldo) as avg_bal_12m,
-  sum(b.saldo * (1 + (a.mesesdc - b.mesesdc)) / 91) as avg_pnd_menos_bal_12m,
-  sum(b.saldo * (13 - (a.mesesdc - b.mesesdc)) / 91) as avg_pnd_mas_bal_12m
+  min(b.saldo) as min_bal_12m,  
+  sum(b.saldo * (1 + (a.mesesdc - b.mesesdc))) / sum(1 + (a.mesesdc - b.mesesdc)) as avg_pnd_menos_bal_12m,
+  sum(b.saldo * (13 - (a.mesesdc - b.mesesdc))) / sum(13 - (a.mesesdc - b.mesesdc))  as avg_pnd_mas_bal_12m,
+  avg(b.saldo) as avg_bal_12m
 from 
   cartera.tbl_vars_bal_step2 a
   left join cartera.tbl_cartera_comercial_rfc_saldo b
-  on a.rfc = b.rfc and b.mesesdc >= a.mesesdc - 6 and b.mesesdc <= a.mesesdc
+  on a.rfc = b.rfc and b.mesesdc >= a.mesesdc - 12 and b.mesesdc <= a.mesesdc
 group by 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17;
 
 create index cartera__tbl_vars_bal_llave_uni_20_idx
