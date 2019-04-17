e6ec80b629d0a800f888a55ff322ad318a87c0d7
rodrigo <rbrionessoto@hotmail.com>
2019-04-17 15:34:33 -0500

    preuniverso y catalogos actualizados

--- mysql/zeppelin/RSS_MCV_UNIVERSO_LLAVES.sql
+++ mysql/zeppelin/RSS_MCV_UNIVERSO_LLAVES.sql
@@ -5,13 +5,16 @@ DROP TABLE IF EXISTS RSS_MCV_UNIVERSO_LLAVES;
 CREATE TABLE  RSS_MCV_UNIVERSO_LLAVES(
 llave_universal  CHAR(20) ,
 fecha           CHAR(10),
-tipo_de_credito   CHAR(100)
+tipo_de_credito   CHAR(100),
+cartera_vencida   double, 
+dias_atraso        int ,
+mesesdc            int
 )
 ROW FORMAT DELIMITED FIELDS TERMINATED BY '|' 
 LOCATION 's3://boi-banregio/datalake/data/InteligenciaRiesgos/M&M/MCV/RSS_MCV_UNIVERSO_LLAVES';
 
 INSERT INTO  RSS_MCV_UNIVERSO_LLAVES
-SELECT  A.LLAVE_UNIVERSAL_20 as llave_universal,A.FECHA, A.TIPO_DE_CREDITO 
+SELECT  A.LLAVE_UNIVERSAL_20 as llave_universal,A.FECHA, A.TIPO_DE_CREDITO ,A.cartera_vencida,A.dias_atraso,A.mesesdc
 FROM RSS_MCV_CARTERA_CREDITICIA_LINEA A 
 INNER JOIN RSS_MCV_COLOCACION_PEQUE B
 ON A.LLAVE_UNIVERSAL_20 = B.llave_universal AND A.fecha = B.fecha
