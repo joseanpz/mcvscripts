e6d17da07188c6abb8e01b72550d623c4d31dba5
rodrigo <rbrionessoto@hotmail.com>
2019-04-23 16:23:18 -0500

    Tablas Comportamiento PyME

---
+++ mysql/zeppelin/RSS_MCV_PEQUE_CRED_LIM_201902.sql
@@ -0,0 +1,11 @@
+%hive
+DROP TABLE IF EXISTS RSS_MCV_PEQUE_CRED_LIM_201902;
+
+CREATE TABLE  RSS_MCV_PEQUE_CRED_LIM_201902
+ROW FORMAT DELIMITED FIELDS TERMINATED BY '|' 
+LOCATION 's3://boi-banregio/datalake/data/InteligenciaRiesgos/M&M/MCV/RSS_MCV_PEQUE_CRED_LIM_201902' AS
+SELECT   b.* 
+FROM JEA_MCV_UNIVERSO a
+LEFT JOIN dbriskdatamart.MZM_MCV_CRED_LIM_201902 b
+on a.fecha = b.fecha and a.rfc=b.rfc
+where a.fecha >=201607 ;
