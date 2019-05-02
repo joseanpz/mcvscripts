1e8806e06ad0443eac651c46a00bc0ea293a3518
rodrigo <rbrionessoto@hotmail.com>
2019-05-02 13:25:44 -0500

    MODIFICACION VARIABLE RSS_MCV_PEQUE_VAR_CR_OP_ACC_13_201902

--- mysql/zeppelin/RSS_MCV_PEQUE_VAR_CR_OP_ACC_13_201902.sql
+++ mysql/zeppelin/RSS_MCV_PEQUE_VAR_CR_OP_ACC_13_201902.sql
@@ -611,7 +611,7 @@ b.avg_pct_venc180m_brg_lse_lsg_op_acc,
 b.avg_pct_venc180m_brg_lse_r_op_acc,
 b.avg_pct_venc180m_brg_lse_cf_op_acc,
 b.avg_pct_venc180m_brg_lse_other_op_acc
-FROM JEA_MCV_UNIVERSO a
+FROM JEA_MCV_UNIVERSO_RFC a
 LEFT JOIN dbriskdatamart.MZM_MCV_VAR_CR_OP_ACC_13_201902 b
 on a.folio_respuesta_bc = b.folio and a.rfc=b.rfc
 where a.fecha >=201607 ;
