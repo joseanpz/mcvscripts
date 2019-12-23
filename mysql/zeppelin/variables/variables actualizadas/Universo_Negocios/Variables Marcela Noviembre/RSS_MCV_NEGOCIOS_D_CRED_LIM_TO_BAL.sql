6b8a436e9ef37a05199cc56f2a601e4792f91298
rodrigo <rbrionessoto@hotmail.com>
2019-12-23 13:16:58 -0600

    Calificación N Noviembre

---
+++ mysql/zeppelin/variables/variables actualizadas/Universo_Negocios/Variables Marcela Noviembre/RSS_MCV_NEGOCIOS_D_CRED_LIM_TO_BAL.sql
@@ -0,0 +1,49 @@
+%hive
+DROP TABLE IF EXISTS RSS_MCV_NEGOCIOS_D_CRED_LIM_TO_BAL;
+
+CREATE TABLE  RSS_MCV_NEGOCIOS_D_CRED_LIM_TO_BAL
+ROW FORMAT DELIMITED FIELDS TERMINATED BY '|' 
+LOCATION 's3://boi-banregio/datalake/data/InteligenciaRiesgos/M&M/MCV/RSS/Noviembre/Variables/RSS_MCV_NEGOCIOS_D_CRED_LIM_TO_BAL' AS
+SELECT   
+'fecha' AS fecha,
+'rfc' as rfc ,
+'d_cred_lim_to_bal' AS d_cred_lim_to_bal,
+'max_d_cred_lim_to_bal_03m' AS max_d_cred_lim_to_bal_03m,
+'min_d_cred_lim_to_bal_03m' AS min_d_cred_lim_to_bal_03m,
+'avg_d_cred_lim_to_bal_03m' AS avg_d_cred_lim_to_bal_03m,
+'avg_pnd_menos_d_cred_lim_to_bal_03m' AS avg_pnd_menos_d_cred_lim_to_bal_03m,
+'avg_pnd_mas_d_cred_lim_to_bal_03m' AS avg_pnd_mas_d_cred_lim_to_bal_03m,
+'max_d_cred_lim_to_bal_06m' AS max_d_cred_lim_to_bal_06m,
+'min_d_cred_lim_to_bal_06m' AS min_d_cred_lim_to_bal_06m,
+'avg_d_cred_lim_to_bal_06m' AS avg_d_cred_lim_to_bal_06m,
+'avg_pnd_menos_d_cred_lim_to_bal_06m' AS avg_pnd_menos_d_cred_lim_to_bal_06m,
+'avg_pnd_mas_d_cred_lim_to_bal_06m' AS avg_pnd_mas_d_cred_lim_to_bal_06m,
+'max_d_cred_lim_to_bal_12m' AS max_d_cred_lim_to_bal_12m,
+'min_d_cred_lim_to_bal_12m' AS min_d_cred_lim_to_bal_12m,
+'avg_d_cred_lim_to_bal_12m' AS avg_d_cred_lim_to_bal_12m,
+'avg_pnd_menos_d_cred_lim_to_bal_12m' AS avg_pnd_menos_d_cred_lim_to_bal_12m,
+'avg_pnd_mas_d_cred_lim_to_bal_12m' AS avg_pnd_mas_d_cred_lim_to_bal_12m;
+INSERT INTO RSS_MCV_NEGOCIOS_D_CRED_LIM_TO_BAL
+SELECT distinct
+a.fecha,
+a.rfc ,
+b.d_cred_lim_to_bal,
+b.max_d_cred_lim_to_bal_03m,
+b.min_d_cred_lim_to_bal_03m,
+b.avg_d_cred_lim_to_bal_03m,
+b.avg_pnd_menos_d_cred_lim_to_bal_03m,
+b.avg_pnd_mas_d_cred_lim_to_bal_03m,
+b.max_d_cred_lim_to_bal_06m,
+b.min_d_cred_lim_to_bal_06m,
+b.avg_d_cred_lim_to_bal_06m,
+b.avg_pnd_menos_d_cred_lim_to_bal_06m,
+b.avg_pnd_mas_d_cred_lim_to_bal_06m,
+b.max_d_cred_lim_to_bal_12m,
+b.min_d_cred_lim_to_bal_12m,
+b.avg_d_cred_lim_to_bal_12m,
+b.avg_pnd_menos_d_cred_lim_to_bal_12m,
+b.avg_pnd_mas_d_cred_lim_to_bal_12m
+FROM RSS_MCV_UNIVERSO_RFC_NEGOCIOS a
+LEFT JOIN dbriskdatamart.MZM_MCV_D_CRED_LIM_TO_BAL_201911 b
+on a.fecha = b.fecha and a.rfc=b.rfc
+WHERE a.fecha = '201911' ;
---
+++ mysql/zeppelin/variables/variables actualizadas/Universo_Negocios/Variables Marcela Noviembre/RSS_MCV_NEGOCIOS_VAR_VARIABLES_DIASATRASO.sql
@@ -0,0 +1,22 @@
+%hive
+DROP TABLE IF EXISTS RSS_MCV_NEGOCIOS_VAR_VARIABLES_DIASATRASO;
+
+CREATE TABLE  RSS_MCV_NEGOCIOS_VAR_VARIABLES_DIASATRASO
+ROW FORMAT DELIMITED FIELDS TERMINATED BY '|' 
+LOCATION 's3://boi-banregio/datalake/data/InteligenciaRiesgos/M&M/MCV/RSS/Noviembre/Variables/RSS_MCV_NEGOCIOS_VAR_VARIABLES_DIASATRASO' AS
+SELECT 
+'fecha' AS fecha,
+'rfc' as rfc , 
+'dias_atraso' AS dias_atraso,
+'dpcv_cap' AS dpcv_cap;
+
+INSERT INTO RSS_MCV_NEGOCIOS_VAR_VARIABLES_DIASATRASO
+SELECT
+a.fecha,
+a.rfc , 
+b.dias_atraso,
+b.dpcv_cap
+FROM RSS_MCV_UNIVERSO_RFC_NEGOCIOS a
+LEFT JOIN dbriskdatamart.MZM_MCV_VAR_VARIABLES_DIASATRASO_201911 b
+on a.rfc=b.rfc and a.fecha=b.fecha
+where a.fecha = '201911';
